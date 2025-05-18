590aad7233782dc28909605a5aa6f3c79d22d711
genesis <isic22.gzapatag@itesco.edu.mx>
2025-05-18 01:39:27 -0600

    arregle relacion entre csss y html del login

--- login.php
+++ login.php
@@ -43,7 +43,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Iniciar Sesión</title>
-    <link rel="stylesheet" href="Bienvenidos.css">
+    <link rel="stylesheet" href="login.css">
     <style>
         .error {
             color: white;
